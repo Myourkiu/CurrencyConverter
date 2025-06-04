64ff7dd70e293cc768024837fa6052599d6a35c9
Pedro Figueiredo <pedrofigsesteves@gmail.com>
2025-06-03 21:42:29 -0300

    feat/picker config

--- App.tsx
+++ App.tsx
@@ -2,16 +2,41 @@ import Container from 'components/Container';
 import './global.css';
 import { Text, View } from 'react-native';
 import PickerItem from 'components/PickerItem';
+import { useEffect, useState } from 'react';
+import { api } from 'service/api';
+import Spinner from 'components/Spinner';
 
 export default function App() {
-  if(loading)
-    return <Spinner/>
+  const [loading, setLoading] = useState(true);
+  const [currency, setCurrency] = useState<any[]>([]);
+  const [selectedCurrency, setSelectedCurrency] = useState(null);
+
+  async function loadCurrency() {
+    const response = await api.get('/all');
+    let currency:any[] = [];
+    Object.keys(response.data).map((key) => {
+      currency.push({
+        key: key,
+        label: key,
+        value: key,
+      });
+    });
+    setCurrency(currency);
+    setSelectedCurrency(currency[0].key);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    loadCurrency();
+  }, []);
+
+  if (loading) return <Spinner />;
 
   return (
     <Container>
       <View className="w-[90%] rounded-t-lg bg-[#f9f9f9] p-2">
         <Text className="pl-1 pt-1 text-base font-medium text-[#000]">Selecione sua moeda</Text>
-        <PickerItem/>
+        <PickerItem currency={currency} selectedCurrency={selectedCurrency} onChange={ (curr) => setSelectedCurrency(curr)}/>
       </View>
     </Container>
   );
--- components/PickerItem.tsx
+++ components/PickerItem.tsx
@@ -1,9 +1,19 @@
 import { View } from "react-native"
 import {Picker} from '@react-native-picker/picker';
-const PickerItem = () => {
+
+interface PickerProps {
+  currency: any[],
+  selectedCurrency: any,
+  onChange: (curr: any) => void
+
+}
+
+const PickerItem = ({currency, selectedCurrency, onChange} : PickerProps) => {
   return (
-    <Picker>
-      <Picker.Item key={0} label="BTC" value={"BTC"}/>
+    <Picker selectedValue={selectedCurrency} onValueChange={(value) => onChange(value)}>
+      {currency.map((item, index) => (
+        <Picker.Item key={index} label={item.key} value={item.key}/>
+      ))}
     </Picker>
   )
 }
